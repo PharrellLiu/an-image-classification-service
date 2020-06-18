1fe147ee7cb7fda7d06957ffece5b2462d89e080
XINYUAN LIU <53179832+PharrellLiu@users.noreply.github.com>
2020-06-18 15:37:41 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,36 @@
+import os
+import random
 import time
 from multiprocessing import Pool
-import os
-processPool = Pool(3)
+from redis import StrictRedis
 
 
-def downloadImage():
-    print(time.localtime(time.time()))
-    print(os.getpid())
-    time.sleep(3)
-    print(time.localtime(time.time()))
+def long_time_task():
+    print('Run task (%s)...' % (os.getpid()))
+    start = time.time()
+    time.sleep(1)
+    end = time.time()
+    print('Task runs %0.2f seconds.' % (end - start))
 
 
-for i in range(10):
-    processPool.apply_async(downloadImage())
-processPool.join()
+if __name__ == '__main__':
+    '''print('Parent process %s.' % os.getpid())
+    p = Pool(4)
+    for i in range(5):
+        p.apply_async(long_time_task)
+
+    while 1:
+        time.sleep(10)'''
+    # Connect and subscribe
+    queue = StrictRedis(host='localhost', port=6379)
+    pubsub = queue.pubsub()
+    pubsub.subscribe('testing')
+    # The first message you receive will be a confirmation of subscription
+    message = pubsub.get_message()
+    print("The first message received:")
+    print(message)
+    p = Pool()
+    while True:
+        message = pubsub.get_message()
+        if message:
+            p.apply_async(long_time_task)
