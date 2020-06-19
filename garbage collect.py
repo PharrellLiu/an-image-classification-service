f83e81879aaa7195b19fab9f4b506fb98b665214
XINYUAN LIU <53179832+PharrellLiu@users.noreply.github.com>
2020-06-19 15:52:55 +0800

    Update garbage collect.py

--- garbage collect.py
+++ garbage collect.py
@@ -1,13 +1,11 @@
 import os
-
 from redis import StrictRedis
 
 if __name__ == '__main__':
     queue = StrictRedis(host='localhost', port=6379)
     pubsub = queue.pubsub()
-    pubsub.subscribe('grabage')
+    pubsub.subscribe('garbage')
 
-    # The first message you receive will be a confirmation of subscription
     message = pubsub.get_message()
     while message is None:
         message = pubsub.get_message()
@@ -15,6 +13,8 @@ if __name__ == '__main__':
     while True:
         message = pubsub.get_message()
         if message:
-            image_name = message['data']
+            image_name = bytes.decode(message['data'])
             if os.path.isfile(image_name):
                 os.remove(image_name)
+            else:
+                pass
